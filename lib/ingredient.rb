cc8ddb1588dc6540f6799b3c46a588ed4b5b5012
Michael Chickillo <truthvendor@gmail.com>
2020-06-03 11:58:37 -0600

    Final commit

---
+++ lib/ingredient.rb
@@ -0,0 +1,16 @@
+class Ingredient
+  attr_reader :name,
+              :unit,
+              :calories
+
+  def initialize(attribute)
+    @name = attribute[:name]
+    @unit = attribute[:unit]
+    @calories = attribute[:calories]
+  end
+
+
+
+
+
+end
